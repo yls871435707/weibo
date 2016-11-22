885ccc114ee142edca020ffaa79f3f18ce7e5a45
Your Name <you@example.com>
2016-11-22 23:29:06 +0800

    cc

--- WeiBoDemo/app/src/main/java/com/yls/weibodemo/MainActivity.java
+++ WeiBoDemo/app/src/main/java/com/yls/weibodemo/MainActivity.java
@@ -1,13 +1,39 @@
 package com.yls.weibodemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView= (ImageView) findViewById(R.id.iv_imageview);
+        AlphaAnimation alphaAnimatiom=new AlphaAnimation(0.1f,1.0f);
+        alphaAnimatiom.setDuration(3000);
+        imageView.setAnimation(alphaAnimatiom);
+        alphaAnimatiom.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
